577943b7238902d360bd1f66a4b5960cced93fb9
dev <dev@ritz.com.mm>
2025-03-19 10:22:37 +0630

    ADD faker

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
