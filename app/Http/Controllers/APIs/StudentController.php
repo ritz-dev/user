12a087c626d85bf8c2711e4f2b1d2b6cdf77cd66
ritz-dev <dev2@ritz.com.mm>
2025-06-04 13:15:15 +0630

    Add

--- app/Http/Controllers/APIs/StudentController.php
+++ app/Http/Controllers/APIs/StudentController.php
@@ -22,6 +22,7 @@ class StudentController extends Controller
     {
         try {
             $validated = $request->validate([
+                'skip' => 'sometimes|integer|min:0|max:100',
                 'limit' => 'sometimes|integer|min:1|max:100',
                 'search' => 'sometimes|string|max:255',
                 'status' => 'sometimes|in:active,resigned,on_leave,terminated',
